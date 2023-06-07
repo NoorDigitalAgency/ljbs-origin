1a8297e26a8e137c80c36d6ecfad454e6d6b5f68
Rojan Gharibpour <sojaner@gmail.com>
2023-06-07 17:41:38 +0200

    Progress

---
+++ site/_data/search.js
@@ -0,0 +1,3 @@
+module.exports = async function () {
+  return (await (await fetch('https://api.json-generator.com/templates/GziA1NMmdRxm/data', {headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer hkbgf5sev7bs4ktubb7nvkba24aruzmf4fcfn0wp'}})).json());
+}
